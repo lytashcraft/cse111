fdd29577a8764278eab4113e6c5fc36255d5f1b5
lytashcraft <thagouveiaf@gmail.com>
2024-10-07 15:28:32 -0600

    FINALLY!

--- test_water_flow.py
+++ test_water_flow.py
@@ -1,5 +1,5 @@
 import pytest
-from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings, reynolds_number, pressure_loss_from_pipe_reduction
 
 def test_water_column_height():
     #the test values
@@ -12,7 +12,7 @@ def test_water_column_height():
 
     for tower_height, tank_height, expected in test_values:
         result = water_column_height(tower_height, tank_height)
-        assert abs(result - expected) < 0.001
+        assert abs(result - expected) < 0.001 # Tolerance of 0.001
 
 def test_pressure_gain_from_water_height():
     #the test values
@@ -25,7 +25,7 @@ def test_pressure_gain_from_water_height():
     for height, expected in test_values:
         result = pressure_gain_from_water_height(height)
         
-        assert abs(result - expected) < 0.001
+        assert abs(result - expected) < 0.001 # Tolerance of 0.001
 
 def test_pressure_loss_from_pipe():
     #the test values
@@ -42,6 +42,48 @@ def test_pressure_loss_from_pipe():
     for pipe_diameter, pipe_length, friction_factor, fluid_velocity, expected in test_values:
         result = pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity)
         
+        assert abs(result - expected) < 0.001 # Tolerance of 0.001
+
+def test_pressure_loss_from_fittings():
+    #the test values
+    test_values = [
+        (0.00, 3, 0.000),
+        (1.65, 0, 0.000),
+        (1.65, 2, -0.109),
+        (1.75, 2, -0.122),
+        (1.75, 5, -0.306)
+    ]
+    
+    for fluid_velocity, quantity_fittings, expected in test_values:
+        result = pressure_loss_from_fittings(fluid_velocity, quantity_fittings)
+        
+        assert abs(result - expected) < 0.001 # Tolerance of 0.001
+
+def test_reynolds_number():
+    #the test values
+    test_values = [
+        (0.048692, 0.00, 0),
+        (0.048692, 1.65, 80069),
+        (0.048692, 1.75, 84922),
+        (0.286870, 1.65, 471729),
+        (0.286870, 1.75, 500318)
+    ]
+    
+    for hydraulic_diameter, fluid_velocity, expected in test_values:
+        result = reynolds_number(hydraulic_diameter, fluid_velocity)
+        
+        assert abs(result - expected) < 1
+
+def test_pressure_loss_from_pipe_reduction():
+
+    test_values = [
+        (0.28687, 0.00, 1, 0.048692, 0.000),
+        (0.28687, 1.65, 471729, 0.048692, -163.744),
+        (0.28687, 1.75, 500318, 0.048692, -184.182)
+    ]
+    
+    for larger_diameter, fluid_velocity, reynolds_number, smaller_diameter, expected in test_values:
+        result = pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter)
         assert abs(result - expected) < 0.001
 
 # Call the main function that is part of pytest so that the
--- water_flow.py
+++ water_flow.py
@@ -19,3 +19,63 @@ def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_v
 
     pressure_loss = - (friction_factor * pipe_length * water_density * fluid_velocity**2) / (2000 * pipe_diameter)
     return pressure_loss
+
+def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
+    water_density = 998.2  # kg/m^3
+
+    pressure_loss = (-0.04 * water_density * fluid_velocity**2 * quantity_fittings) / 2000
+
+    return pressure_loss
+
+def reynolds_number(hydraulic_diameter, fluid_velocity):
+    water_density = 998.2  # kg/m³
+    dynamic_viscosity = 0.0010016
+
+    r_number = (water_density * hydraulic_diameter * fluid_velocity) / dynamic_viscosity
+
+    return r_number
+
+def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
+    water_density = 998.2  # kg/m³
+
+    # Calculate the constant k
+    k = (0.1 + (50 / reynolds_number)) * (((larger_diameter / smaller_diameter) ** 4) - 1)
+
+    # Calculate the pressure loss
+    pressure_loss = (-k * water_density * fluid_velocity ** 2) / 2000
+
+    return pressure_loss
+
+PVC_SCHED80_INNER_DIAMETER = 0.28687 # (meters)  11.294 inches
+PVC_SCHED80_FRICTION_FACTOR = 0.013  # (unitless)
+SUPPLY_VELOCITY = 1.65               # (meters / second)
+HDPE_SDR11_INNER_DIAMETER = 0.048692 # (meters)  1.917 inches
+HDPE_SDR11_FRICTION_FACTOR = 0.018   # (unitless)
+HOUSEHOLD_VELOCITY = 1.75            # (meters / second)
+def main():
+    tower_height = float(input("Height of water tower (meters): "))
+    tank_height = float(input("Height of water tank walls (meters): "))
+    length1 = float(input("Length of supply pipe from tank to lot (meters): "))
+    quantity_angles = int(input("Number of 90° angles in supply pipe: "))
+    length2 = float(input("Length of pipe from supply to house (meters): "))
+    water_height = water_column_height(tower_height, tank_height)
+    pressure = pressure_gain_from_water_height(water_height)
+    diameter = PVC_SCHED80_INNER_DIAMETER
+    friction = PVC_SCHED80_FRICTION_FACTOR
+    velocity = SUPPLY_VELOCITY
+    reynolds = reynolds_number(diameter, velocity)
+    loss = pressure_loss_from_pipe(diameter, length1, friction, velocity)
+    pressure += loss
+    loss = pressure_loss_from_fittings(velocity, quantity_angles)
+    pressure += loss
+    loss = pressure_loss_from_pipe_reduction(diameter,
+            velocity, reynolds, HDPE_SDR11_INNER_DIAMETER)
+    pressure += loss
+    diameter = HDPE_SDR11_INNER_DIAMETER
+    friction = HDPE_SDR11_FRICTION_FACTOR
+    velocity = HOUSEHOLD_VELOCITY
+    loss = pressure_loss_from_pipe(diameter, length2, friction, velocity)
+    pressure += loss
+    print(f"Pressure at house: {pressure:.1f} kilopascals")
+if __name__ == "__main__":
+    main()
